c93a15ee9a57fc3544556a464f19f038b88eef65
rabi777 <rabi375306@gmail.com>
2022-06-07 15:26:04 +0600

    feat: new doctype customer source

---
+++ bbb/bbb/doctype/customer_source/customer_source.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, invento software limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Source', {
+	// refresh: function(frm) {
+
+	// }
+});
