010bf86d5b1cbef98ccc47c25f21935a4e54a4c2
rabi777 <rabi375306@gmail.com>
2022-06-22 18:00:55 +0600

    perf: customer date_of_birth added

--- bbb/public/js/customer_quick_entry.js
+++ bbb/public/js/customer_quick_entry.js
@@ -9,6 +9,38 @@ frappe.ui.form.CustomerQuickEntryForm = frappe.ui.form.QuickEntryForm.extend({
 	render_dialog: function() {
 		frappe.model.set_value('Customer', this.doc.name, 'mobile_number', this.doc.customer_name);
 		frappe.model.set_value('Customer', this.doc.name, 'customer_name', null);
+		this.mandatory = this.mandatory.concat(this.get_variant_fields());
 		this._super();
+	},
+	get_variant_fields: function() {
+		var variant_fields = [{
+			fieldtype: "Section Break",
+			label: __("Date Of Birth"),
+			collapsible: 0
+		},
+		{
+			label: __("Day"),
+			fieldname: "day",
+			fieldtype: "Int"
+		},
+		{
+			fieldtype: "Column Break"
+		},
+		{
+			label: __("Month"),
+			fieldname: "month",
+			fieldtype: "Select",
+			options: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+		},
+		{
+			fieldtype: "Column Break"
+		},
+		{
+			label: __("Year"),
+			fieldname: "year",
+			fieldtype: "Int"
+		},];
+
+		return variant_fields;
 	},
 });
