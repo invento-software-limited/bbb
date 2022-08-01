35754a9f545fa92f15ed353e053dfc6a7ec4cbab
rabi777 <rabi375306@gmail.com>
2022-08-01 16:31:36 +0600

    fix: customer import bug fixed

--- bbb/bbb/doctype/customer_bulk_import/customer_bulk_import.py
+++ bbb/bbb/doctype/customer_bulk_import/customer_bulk_import.py
@@ -35,17 +35,26 @@ def customer_bulk_import_xl(doc_name):
     i = 0
     for row in data:
         if i > 0:
-            customer_new_doc = frappe.new_doc("Customer")
-            customer_new_doc.customer_name = row[0].value
-            customer_new_doc.mobile_number = row[2].value
-            customer_new_doc.customer_type = row[3].value
-            customer_new_doc.customer_group = row[4].value
-            customer_new_doc.territory = row[5].value
-            customer_new_doc.purchase_amount = row[6].value
-            customer_new_doc.pos_profile = row[7].value
-            customer_new_doc.save()
-            customer_new_doc.customer_name = row[1].value
-            customer_new_doc.save()
-
+            # customer_new_doc = frappe.new_doc("Customer")
+            # customer_new_doc.customname = row[0].value
+            # customer_new_doc.mobile_number = row[2].value
+            # customer_new_doc.customer_type = row[3].value
+            # customer_new_doc.customer_group = row[4].value
+            # customer_new_doc.territory = row[5].value
+            # customer_new_doc.purchase_amount = row[6].value
+            # customer_new_doc.pos_profile = row[7].value
+            # customer_new_doc.save()
+            # customer_new_doc.customer_name = row[1].value
+            # customer_new_doc.save()
+            try:
+                sql = """INSERT INTO `tabCustomer`(name, customer_name, mobile_number, customer_type, customer_group, territory, purchase_amount, pos_profile)
+                VALUES('{}','{}','{}','{}','{}','{}','{}','{}')""".format(
+                    row[0].value, row[1].value, row[2].value, row[3].value, row[4].value, row[5].value, row[6].value,
+                    row[7].value)
+                frappe.db.sql(sql)
+            except:
+                pass
+        # if i == 5:
+        #     break
         i += 1
     return {"message": "Import Done"}
