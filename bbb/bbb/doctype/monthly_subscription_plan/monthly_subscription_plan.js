2931a10ae86f2a15a19c5008fd8eb50364aa68c9
rabi777 <rabiul737@invento.com.bd>
2022-12-06 10:10:43 +0600

    Implement Subscription

---
+++ bbb/bbb/doctype/monthly_subscription_plan/monthly_subscription_plan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Invento BD and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Monthly Subscription Plan', {
+	// refresh: function(frm) {
+
+	// }
+});
