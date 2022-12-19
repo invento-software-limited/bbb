138861b28ec642b288718f2274fffcd88be54f66
rabi777 <rabiul737@invento.com.bd>
2022-12-19 15:30:12 +0600

    update: add price rule tag doctype

---
+++ bbb/bbb/doctype/price_rule_tag/price_rule_tag.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, invento software limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Price Rule Tag', {
+	// refresh: function(frm) {
+
+	// }
+});
