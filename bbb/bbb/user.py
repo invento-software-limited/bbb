3199668d8373334db1a0183af74079ba85fdd112
rabi777 <rabi375306@gmail.com>
2023-05-19 08:18:02 +0600

    feat: service person weight validation and user company field hooks'

---
+++ bbb/bbb/user.py
@@ -0,0 +1,22 @@
+import frappe
+
+def validate(doc, method):
+    user_permission = frappe.db.exists("User Permission", {'user': doc.email, 'allow': 'Company'})
+    if user_permission:
+        permission_doc = frappe.get_doc("User Permission", user_permission)
+        if doc.company and doc.company != permission_doc.for_value:
+            permission_doc = frappe.get_doc("User Permission", user_permission)
+            permission_doc.for_value = doc.company
+            permission_doc.save()
+        else:
+            permission_doc.delete()
+    elif doc.company:
+        new_doc = frappe.new_doc('User Permission')
+        new_doc.user = doc.email
+        new_doc.allow = 'Company'
+        new_doc.for_value = doc.company
+        new_doc.insert()
+        new_doc.save()
+
+    print(user_permission)
+
