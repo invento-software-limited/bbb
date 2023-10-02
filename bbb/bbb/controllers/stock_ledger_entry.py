48b1a48797f035e990337be248b7d6dda3bb090b
MD. Rabiul Islam <rabi375306@invento.com.bd>
2023-10-02 12:51:41 +0600

    update: woocommerce stock ledger entry updated

--- bbb/bbb/controllers/stock_ledger_entry.py
+++ bbb/bbb/controllers/stock_ledger_entry.py
@@ -3,6 +3,8 @@
 
 
 import frappe
+from frappe.utils import flt
+
 
 from erpnext.stock.doctype.stock_ledger_entry.stock_ledger_entry import StockLedgerEntry
 
@@ -11,24 +13,24 @@ class CustomStockLedgerEntry(StockLedgerEntry):
 		super(CustomStockLedgerEntry, self).__init__(*args, **kwargs)
   
   
-	def on_submit(self):
-		super(CustomStockLedgerEntry, self).on_submit()
+	def on_update(self):
+		# super(CustomStockLedgerEntry, self).on_submit()
   
 		woocommerce_settings = frappe.get_doc("Woocommerce Settings")
 		item_doc = frappe.get_doc('Item', {'item_code': self.item_code})
 
-		if item_doc.woocommerce_id:
+		if item_doc.woocommerce_id and woocommerce_settings.warehouse == self.warehouse:
 			from woocommerce import API
 			wcapi = API(
 				url=woocommerce_settings.woocommerce_server_url,
 				consumer_key=woocommerce_settings.api_consumer_key,
 				consumer_secret=woocommerce_settings.api_consumer_secret,
 				wp_api=True,
-				version="wc/v1",
+				version="wc/v3",
 				query_string_auth=True,
 			)
 			data = {
-				"stock_quantity": str(self.qty_after_transaction),
+				"stock_quantity": self.qty_after_transaction,
 				"manage_stock": True
 			}
 			url = "products/" + str(item_doc.woocommerce_id)
